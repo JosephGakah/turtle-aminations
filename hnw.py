f63a44871e540a4c8d1ab249382433e406305a20
JosephGakah <joegakah@gmail.com>
2022-12-31 22:35:02 +0300

    turtle merry christmass

---
+++ hnw.py
@@ -0,0 +1,444 @@
+import turtle
+
+turtle.setup(1000,1000,0,0)
+
+hnwtul=turtle.Turtle()
+hnwtul.width(8)
+hnwtul.color("red")
+new=turtle.getscreen()
+hnwtul.speed(4)
+
+new.bgcolor("lightblue")
+
+# Hidden Work(penup)
+hnwtul.left(180)
+hnwtul.penup()
+hnwtul.forward(300)
+hnwtul.right(90)
+hnwtul.forward(100)
+hnwtul.pendown()
+
+# Printing H
+
+
+#start to draw
+hnwtul.forward(50)
+hnwtul.right(90)
+hnwtul.forward(50)
+hnwtul.left(90)
+hnwtul.forward(50)
+hnwtul.left(90)
+
+hnwtul.penup()
+hnwtul.forward(50)
+hnwtul.left(90)
+hnwtul.pendown()
+hnwtul.forward(50)
+hnwtul.left(90)
+hnwtul.forward(50)
+hnwtul.right(90)
+hnwtul.forward(50)
+
+
+# printing A
+
+hnwtul.penup()
+hnwtul.left(90)
+hnwtul.forward(15)
+hnwtul.pendown()
+hnwtul.left(70)
+hnwtul.forward(110)
+hnwtul.right(70)
+hnwtul.right(70)
+hnwtul.forward(110)
+hnwtul.left(180)
+hnwtul.forward(55)
+hnwtul.left(70)
+hnwtul.forward(38)
+hnwtul.left(70)
+hnwtul.penup()
+hnwtul.forward(55)
+hnwtul.left(110)
+
+hnwtul.forward(100)
+
+# printing P
+
+hnwtul.left(90)
+hnwtul.pendown()
+hnwtul.forward(100)
+hnwtul.right(90)
+hnwtul.forward(50)
+hnwtul.right(20)
+hnwtul.forward(20)
+hnwtul.right(70)
+hnwtul.forward(40)
+hnwtul.right(70)
+hnwtul.forward(20)
+hnwtul.right(20)
+hnwtul.forward(50)
+hnwtul.left(90)
+hnwtul.forward(50)
+hnwtul.left(90)
+hnwtul.penup()
+hnwtul.forward(100)
+
+
+# printing P
+
+hnwtul.left(90)
+hnwtul.pendown()
+hnwtul.forward(100)
+hnwtul.right(90)
+hnwtul.forward(50)
+hnwtul.right(20)
+hnwtul.forward(20)
+hnwtul.right(70)
+hnwtul.forward(40)
+hnwtul.right(70)
+hnwtul.forward(20)
+hnwtul.right(20)
+hnwtul.forward(50)
+hnwtul.left(90)
+hnwtul.forward(50)
+hnwtul.left(90)
+hnwtul.penup()
+hnwtul.forward(100)
+
+# printing Y
+
+hnwtul.forward(20)
+hnwtul.pendown()
+hnwtul.left(90)
+hnwtul.forward(50)
+hnwtul.left(30)
+hnwtul.forward(60)
+hnwtul.backward(60)
+hnwtul.right(60)
+hnwtul.forward(60)
+hnwtul.backward(60)
+hnwtul.left(30)
+
+# go to Home
+
+hnwtul.penup()
+hnwtul.home()
+
+hnwtul.color("orange")
+new.bgcolor("lightgreen")
+# setting second row
+
+hnwtul.backward(300)
+hnwtul.right(90)
+hnwtul.forward(60)
+hnwtul.left(180)
+
+
+# printing B
+
+
+hnwtul.pendown()
+hnwtul.forward(100)
+hnwtul.right(160)
+hnwtul.forward(100)
+hnwtul.left(160)
+hnwtul.forward(100)
+
+
+
+# go to Home
+
+hnwtul.penup()
+hnwtul.home()
+
+# setting up
+
+hnwtul.backward(240)
+hnwtul.right(90)
+hnwtul.forward(10)
+hnwtul.pendown()
+hnwtul.forward(50)
+hnwtul.left(90)
+hnwtul.forward(50)
+hnwtul.backward(50)
+hnwtul.left(90)
+hnwtul.forward(100)
+hnwtul.right(90)
+hnwtul.forward(50)
+hnwtul.backward(50)
+hnwtul.right(90)
+hnwtul.forward(50)
+hnwtul.left(90)
+hnwtul.forward(50)
+hnwtul.penup()
+hnwtul.home()
+
+# set up for W
+
+hnwtul.backward(150)
+hnwtul.right(90)
+hnwtul.forward(60)
+hnwtul.pendown()
+hnwtul.backward(100)
+hnwtul.forward(100)
+hnwtul.left(120)
+hnwtul.forward(40)
+hnwtul.right(60)
+hnwtul.forward(40)
+hnwtul.left(120)
+hnwtul.forward(100)
+hnwtul.penup()
+hnwtul.home()
+
+# set up for Y
+
+hnwtul.backward(30)
+hnwtul.right(90)
+hnwtul.forward(65)
+hnwtul.left(90)
+
+
+
+# set up for Y
+
+
+hnwtul.pendown()
+hnwtul.left(90)
+hnwtul.forward(50)
+hnwtul.left(30)
+hnwtul.forward(60)
+hnwtul.backward(60)
+hnwtul.right(60)
+hnwtul.forward(60)
+hnwtul.backward(60)
+hnwtul.left(30)
+
+# go to Home 
+
+hnwtul.penup()
+hnwtul.home()
+
+
+# printing E
+
+hnwtul.forward(10)
+hnwtul.right(90)
+hnwtul.forward(10)
+hnwtul.pendown()
+hnwtul.forward(50)
+hnwtul.left(90)
+hnwtul.forward(50)
+hnwtul.backward(50)
+hnwtul.left(90)
+hnwtul.forward(100)
+hnwtul.right(90)
+hnwtul.forward(50)
+hnwtul.backward(50)
+hnwtul.right(90)
+hnwtul.forward(50)
+hnwtul.left(90)
+hnwtul.forward(50)
+hnwtul.penup()
+hnwtul.home()
+
+
+# set up for A
+hnwtul.forward(90)
+
+# printing A
+
+hnwtul.right(90)
+hnwtul.forward(50)
+hnwtul.left(90)
+hnwtul.pendown()
+hnwtul.left(70)
+hnwtul.forward(110)
+hnwtul.right(70)
+hnwtul.right(70)
+hnwtul.forward(110)
+hnwtul.left(180)
+hnwtul.forward(55)
+hnwtul.left(70)
+hnwtul.forward(38)
+hnwtul.left(70)
+hnwtul.penup()
+hnwtul.forward(55)
+hnwtul.left(110)
+
+hnwtul.forward(100)
+
+
+#go to home
+hnwtul.penup()
+hnwtul.home()
+
+
+# set up for R
+hnwtul.forward(180)
+hnwtul.right(90)
+hnwtul.forward(50)
+hnwtul.left(180)
+
+#set up for R
+hnwtul.pendown()
+hnwtul.forward(100)
+hnwtul.right(90)
+hnwtul.forward(50)
+hnwtul.right(20)
+hnwtul.forward(20)
+hnwtul.right(70)
+hnwtul.forward(40)
+hnwtul.right(70)
+hnwtul.forward(20)
+hnwtul.right(20)
+hnwtul.forward(50)
+hnwtul.left(180)
+hnwtul.forward(50)
+hnwtul.right(20)
+hnwtul.forward(20)
+hnwtul.right(70)
+hnwtul.forward(40)
+hnwtul.left(180)
+hnwtul.penup()
+hnwtul.home()
+
+# setting position
+
+hnwtul.right(90)
+hnwtul.forward(215)
+hnwtul.right(90)
+hnwtul.forward(170)
+
+
+#color
+
+hnwtul.color("green")
+new.bgcolor("lightblue")
+
+
+# printing 2
+
+
+hnwtul.pendown()
+hnwtul.forward(60)
+hnwtul.right(90)
+hnwtul.forward(40)
+hnwtul.right(90)
+hnwtul.forward(50)
+hnwtul.left(20)
+hnwtul.forward(20)
+hnwtul.left(70)
+hnwtul.forward(30)
+hnwtul.left(20)
+hnwtul.forward(20)
+hnwtul.left(70)
+hnwtul.forward(60)
+
+
+#printing 0
+hnwtul.penup()
+hnwtul.home()
+hnwtul.right(90)
+hnwtul.forward(170)
+hnwtul.right(90)
+hnwtul.forward(50)
+hnwtul.right(90)
+
+hnwtul.pendown()
+hnwtul.circle(40)
+
+# go to homw
+
+hnwtul.penup()
+hnwtul.home()
+
+
+# Printing 1
+
+hnwtul.right(90)
+hnwtul.forward(215)
+hnwtul.right(180)
+
+# Set up for 1
+
+hnwtul.pendown()
+hnwtul.forward(100)
+hnwtul.left(90)
+hnwtul.forward(10)
+hnwtul.left(20)
+hnwtul.forward(20)
+
+#go to home
+
+hnwtul.penup()
+hnwtul.home()
+
+# setup for 8
+hnwtul.right(90)
+hnwtul.forward(160)
+hnwtul.left(90)
+hnwtul.forward(70)
+hnwtul.pendown()
+hnwtul.circle(35)
+hnwtul.right(180)
+hnwtul.circle(35)
+
+#go to home()
+hnwtul.penup()
+hnwtul.home()
+
+
+#design pattern
+hnwtul.home()
+hnwtul.forward(370)
+hnwtul.pendown()
+hnwtul.color("hotpink")
+hnwtul.width(3)
+hnwtul.speed(0)
+
+def squre(length, angle):
+    
+    hnwtul.forward(length)
+    hnwtul.right(angle)
+    hnwtul.forward(length)
+    hnwtul.right(angle)
+   
+    hnwtul.forward(length)
+    hnwtul.right(angle)
+    hnwtul.forward(length)
+    hnwtul.right(angle)
+
+squre(80, 90)
+
+for i in range(36):
+      hnwtul.right(10)
+      squre(80, 90)
+
+
+
+hnwtul.penup()
+hnwtul.home()
+hnwtul.left(90)
+hnwtul.forward(270)
+hnwtul.left(90)
+hnwtul.forward(200)
+hnwtul.pendown()
+
+def draw_star(size, color):
+    angle = 120
+    hnwtul.fillcolor(color)
+    hnwtul.begin_fill()
+
+    for side in range(5):
+        hnwtul.forward(size)
+        hnwtul.right(angle)
+        hnwtul.forward(size)
+        hnwtul.right(72 - angle)
+    hnwtul.end_fill()
+    return
+
+draw_star(30, "purple")
+
+
+
+turtle.mainloop()
